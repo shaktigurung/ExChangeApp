3eee6e75bacf38c98c0dd45a4cc4963688d053aa
shakti gurung <mr.shakti.gurung@gmail.com>
2018-10-24 15:56:52 +1100

    AWS added

--- app/uploaders/main_image_uploader.rb
+++ app/uploaders/main_image_uploader.rb
@@ -1,12 +1,14 @@
 class MainImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   # storage :fog
-
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
